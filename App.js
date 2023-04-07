4064e3971d614fcde174563874b3ceffff2be1ab
Greg <greg@sheb.rocks>
2023-04-07 13:15:41 +0200

    counter implementation

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+
+    const [count, setCount] = useState(0)
+
   return (
+
+
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is a manually created textfield in iOS</Text>
+        <Text>{count}</Text>
+        <Button title={'counter'} onPress={() => setCount(count +1)}>Plus 1</Button>
       <StatusBar style="auto" />
     </View>
   );
